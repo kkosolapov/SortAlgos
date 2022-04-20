886320a5cfd85dcd3c29f030f1c037f1d63fd394
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 17:26:08 +0300

    Update simple_exchange_sort.swift

--- simple_exchange_sort.swift
+++ simple_exchange_sort.swift
@@ -1,26 +1,34 @@
-void SimpleExchangeSort() {
+import Foundation
 
-    int n;
+func SimpleExchangeSort() {
+
+    var n;
 
     try {
         cout << "Enter positive size of array\n";
         cin >> n;
 
-        if (n < 1 || n != int(n)) {
-            throw "Size of array must be a positive int number";
-        }
+        do {
+        try 
+             print ("Enter positive size of array\n");
+             n=readline()
+            
+            if n < 1 {
+                print ("Size of array must be a positive int number")
+            }
+       
+            else {
+                print("\n")
 
-        else {
-            cout << '\n';
+            var arr = [Int](n);
 
-            int* arr = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                cin >> arr[i];
+            for i in  0...n {
+                arr[i] = readline();
             }
 
-            for (int i = 1; i < n; i++) {
-                for (int j = n - 1; j >= i; j--) {
+
+            for  i = 1...n {
+                for j = n - 1...i {
                     if (arr[j] < arr[j - 1]) {
                         int r = arr[j];
                         arr[j] = arr[j - 1];
@@ -29,20 +37,21 @@ void SimpleExchangeSort() {
                 }
             }
 
-            cout << '\n';
+            print ("\n");
 
-            cout << "Sorted array\n";
-            for (int i = 0; i < n; i++) {
-                cout << arr[i];
+            print("Sorted array\n");
+            for  i in  0...n {
+                print(arr[i]);
             }
 
-            delete[]arr;
-            cout << '\n';
+            print("\n");
         }
 
     }
 
-    catch (int i) {
-        cin >> n;
+    catch let i{
+        n = readline;
     }
 }
+
+SimpleExchangeSort()

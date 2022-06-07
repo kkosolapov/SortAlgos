ddad59b0dfe8ec1a8c75cdecc82387a2b88a5367
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-07 07:29:04 +0300

    Update insertion_sort.swift

--- insertion_sort.swift
+++ insertion_sort.swift
@@ -2,11 +2,11 @@ import Foundation
 
 func InsertionSort() {
 
-    var n;
+    var n
 
     do {
         try 
-             print ("Enter positive size of array\n");
+             print ("Enter positive size of array\n")
              n=readline()
             
             if n < 1 {
@@ -16,22 +16,22 @@ func InsertionSort() {
             else {
                 print("\n")
 
-            var arr = [Int](n);
+            var arr = [Int](n)
 
             for i in  0...n {
-                arr[i] = readline();
+                arr[i] = readline()
             }
 
-            var i = 0;
+            var i = 0
 
             for j in 1...n{
-                var key = arr[j];
-                i = j - 1;
+                var key = arr[j]
+                i = j - 1
 
                 while i >= 0 && arr[i] > key {
-                    arr[i + 1] = arr[i];
-                    i = i - 1;
-                    arr[i + 1] = key;
+                    arr[i + 1] = arr[i]
+                    i = i - 1
+                    arr[i + 1] = key
                 }
 
             }
@@ -40,17 +40,17 @@ func InsertionSort() {
 
             print("Sorted array\n")
             for i in 0...n {
-                print(arr[i]);
+                print(arr[i])
             }
                 
-            print("\n");
+            print("\n")
         
         }
        
     }
 
     catch let i{
-        n = readline;
+        n = readline()
     }
 }
 
